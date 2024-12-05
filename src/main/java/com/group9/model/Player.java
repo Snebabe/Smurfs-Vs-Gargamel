f091b4bf1ff1b41c8704edfce551df5329ee5d23
emil <emil.bergstrom04@gmail.com>
2024-12-05 12:14:45 +0100

    Changes by nilsg

--- src/main/java/com/group9/model/Player.java
+++ src/main/java/com/group9/model/Player.java
@@ -5,10 +5,20 @@ import com.group9.model.entities.defenders.DefenceEntity;
 public class Player {
     private int resources;
     private DefenceEntity selectedDefence;
+    private int currency;
 
     Player(){
         this.resources = 0;
         this.selectedDefence = null;
+        this.currency = 0;
+    }
+
+    public int getCurrency() {
+        return currency;
+    }
+    public int changeCurrency(int change) {
+        this.currency += change;
+        return change;
     }
 
 }
